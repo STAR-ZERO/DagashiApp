ba8488ccdf6a8926bf886025334b81bd10ab33a9
Kenji Abe <kenji.01.star@gmail.com>
2020-10-29 20:07:08 +0900

    Update AGP, Compose

--- buildSrc/build_dependencies.gradle.kts
+++ buildSrc/build_dependencies.gradle.kts
@@ -1,4 +1,4 @@
 extra.apply {
-    set("android_gradle_plugin_version", "4.2.0-alpha14")
+    set("android_gradle_plugin_version", "4.2.0-alpha15")
     set("kotlin_version", "1.4.10")
 }
--- buildSrc/src/main/kotlin/com/star_zero/dagashi/build/dependecies/Versions.kt
+++ buildSrc/src/main/kotlin/com/star_zero/dagashi/build/dependecies/Versions.kt
@@ -6,5 +6,5 @@ object Versions {
 
     val ANDROID_GRADLE_PLUGIN get() = androidGradlePlugin
     val KOTLIN get() = kotlin
-    const val COMPOSE = "1.0.0-alpha05"
+    const val COMPOSE = "1.0.0-alpha06"
 }
