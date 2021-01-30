caf6833f7c021174674782eb5042b0d9578cfd30
Kenji Abe <kenji.01.star@gmail.com>
2021-01-30 11:23:03 +0900

    Update AGP 4.2.0-beta04

--- buildSrc/build_dependencies.gradle.kts
+++ buildSrc/build_dependencies.gradle.kts
@@ -1,5 +1,5 @@
 extra.apply {
-    set("android_gradle_plugin_version", "4.2.0-beta03")
+    set("android_gradle_plugin_version", "4.2.0-beta04")
     set("kotlin_version", "1.4.21")
     set("compose_version", "1.0.0-alpha09")
 }
