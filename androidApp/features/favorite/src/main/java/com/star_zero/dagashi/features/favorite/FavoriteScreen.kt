a3067c506f472a08b19a4eb84c7f8e945e2b2e72
Kenji Abe <kenji.01.star@gmail.com>
2022-04-29 20:39:46 +0900

    Show delete confirmation dialog

--- androidApp/features/favorite/src/main/java/com/star_zero/dagashi/features/favorite/FavoriteScreen.kt
+++ androidApp/features/favorite/src/main/java/com/star_zero/dagashi/features/favorite/FavoriteScreen.kt
@@ -5,18 +5,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -26,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.spec.DestinationStyle
+import com.star_zero.dagashi.core.CoreString
 import com.star_zero.dagashi.core.ui.components.IssueCard
 import com.star_zero.dagashi.core.usecase.OpenLinkUseCase
 import com.star_zero.dagashi.shared.model.Issue
@@ -49,7 +55,7 @@ fun FavoriteScreen(
             openLinkUseCase(it, viewModel.uiState.isOpenLinkInApp)
         },
         onClickFavorite = viewModel::toggleFavorite,
-        onClickDeleteAll = viewModel::deleteAllFavorites,
+        onDeleteAll = viewModel::deleteAllFavorites,
         onClickBack = navigator::navigateBack
     )
 }
@@ -59,14 +65,14 @@ private fun FavoriteContainer(
     uiState: FavoriteUiState,
     onOpenLink: (String) -> Unit,
     onClickFavorite: (Issue, String, Boolean) -> Unit,
-    onClickDeleteAll: () -> Unit,
+    onDeleteAll: () -> Unit,
     onClickBack: () -> Unit
 ) {
     Surface(color = MaterialTheme.colors.background) {
         Scaffold(
             topBar = {
                 AppBar(
-                    onClickDeleteAll = onClickDeleteAll,
+                    onDeleteAll = onDeleteAll,
                     onClickBack = onClickBack
                 )
             }
@@ -82,7 +88,7 @@ private fun FavoriteContainer(
 
 @Composable
 private fun AppBar(
-    onClickDeleteAll: () -> Unit,
+    onDeleteAll: () -> Unit,
     onClickBack: () -> Unit
 ) {
     TopAppBar(
@@ -95,11 +101,48 @@ private fun AppBar(
             }
         },
         actions = {
-            IconButton(onClick = onClickDeleteAll) {
-                Icon(Icons.Filled.Delete, "Delete")
-            }
+            DeleteAllButton(
+                onDeleteAll = onDeleteAll
+            )
         }
     )
+}
+
+@Composable
+private fun DeleteAllButton(
+    onDeleteAll: () -> Unit,
+) {
+
+    var isShowConfirmation by remember { mutableStateOf(false) }
+    if (isShowConfirmation) {
+        AlertDialog(
+            onDismissRequest = { isShowConfirmation = false },
+            text = {
+                Text(
+                    text = stringResource(id = R.string.favorite_delete_all)
+                )
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    onDeleteAll()
+                    isShowConfirmation = false
+                }) {
+                    Text(text = stringResource(id = CoreString.ok))
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    isShowConfirmation = false
+                }) {
+                    Text(text = stringResource(id = CoreString.cancel))
+                }
+            }
+        )
+    }
+
+    IconButton(onClick = { isShowConfirmation = true }) {
+        Icon(Icons.Filled.Delete, "Delete")
+    }
 }
 
 @Composable
