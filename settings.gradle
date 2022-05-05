fc9b896c1beefc3d7ab571e16645ea4d0c99632d
Kenji Abe <kenji.01.star@gmail.com>
2022-05-05 13:50:04 +0900

    Update

--- settings.gradle
+++ settings.gradle
@@ -1,11 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
