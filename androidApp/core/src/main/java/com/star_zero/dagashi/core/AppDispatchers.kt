86b441420fef54540677779d2529c7ae116fdd8a
Kenji Abe <kenji.01.star@gmail.com>
2022-05-05 16:32:51 +0900

    test ktlint

--- androidApp/core/src/main/java/com/star_zero/dagashi/core/AppDispatchers.kt
+++ androidApp/core/src/main/java/com/star_zero/dagashi/core/AppDispatchers.kt
@@ -3,7 +3,7 @@ package com.star_zero.dagashi.core
 import kotlinx.coroutines.CoroutineDispatcher
 
 interface AppDispatchers {
-    val main: CoroutineDispatcher
-    val io: CoroutineDispatcher
-    val default: CoroutineDispatcher
+    val main:CoroutineDispatcher
+    val io:CoroutineDispatcher
+    val default:CoroutineDispatcher
 }
