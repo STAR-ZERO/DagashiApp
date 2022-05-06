5c635a74e88f1dd2805a5c18078caf6ea611e7b2
Kenji Abe <kenji.01.star@gmail.com>
2022-05-06 09:42:28 +0900

    Fix ktlint

--- shared/src/commonMain/kotlin/com/star_zero/dagashi/shared/platform/CoroutineDispatchers.kt
+++ shared/src/commonMain/kotlin/com/star_zero/dagashi/shared/platform/CoroutineDispatchers.kt
@@ -2,7 +2,7 @@ package com.star_zero.dagashi.shared.platform
 
 import kotlinx.coroutines.CoroutineDispatcher
 
-expect object CoroutineDispatchers  {
+expect object CoroutineDispatchers {
     val main: CoroutineDispatcher
     val io: CoroutineDispatcher
 }
--- shared/src/iosMain/kotlin/com/star_zero/dagashi/shared/platform/CoroutineDispatchers.kt
+++ shared/src/iosMain/kotlin/com/star_zero/dagashi/shared/platform/CoroutineDispatchers.kt
@@ -3,7 +3,7 @@ package com.star_zero.dagashi.shared.platform
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
-actual object CoroutineDispatchers  {
+actual object CoroutineDispatchers {
     actual val main: CoroutineDispatcher = Dispatchers.Main
     actual val io: CoroutineDispatcher = Dispatchers.Default
 }
